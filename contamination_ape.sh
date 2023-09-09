53b29103fb3c8c08575f587949bd767f81210918
kuhlwilm <79041549+kuhlwilm@users.noreply.github.com>
2023-09-09 17:11:40 +0200

    Update contamination_ape.sh

--- contamination_ape.sh
+++ contamination_ape.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-# @ job_name = contam
-# @ initialdir = ~/HuConTest
-# @ total_tasks = 1
-# @ cpus_per_task = 1
-# @ wall_clock_limit = 1:00:00
-# @ class = normal
-# @ array = 1
+#
+#SBATCH --job-name=contam
+#SBATCH --cpus-per-task=1
+#SBATCH -N 1
+#SBATCH --time=1:00:00
+#SBATCH --output=~/logs/%j-%a.out
+#SBATCH --error=~/logs/%j-%a.err
 
-## these modules might be different depending on the system
+## these modules might be different depending on the system, e.g.
 module load bcftools/1.18 --auto
 module load R/4.3.0 --auto
 
 ## this should be executed directly in the directory of the HuConTest script
 # cd HuConTest/
 
-# CAUTION 1: Different versions of SAMTOOLS work differently. (Same might be true for R, but the commands are fairly standard in this case.)
-# For example, SAMTOOLS 1.0 does not take CRAM files, so it will not give any output. SAMTOOLS 1.9 can use such files. Only these two versions were tested.
-# It is advisable to use BAM files, if possible, since this goes faster.
-## IMPORTANT UPGRADE on this matter: samtools mpileup is deprecated in more recent versions, instead bcftools mpileup is used now.
+# CAUTION 1: Different versions of the tools work differently, it might be necessary to use a bcftools version close to 1.18, and R >=3.2.0.
+# It is advisable to use BAM files, if possible, since this goes faster. But CRAM files are also supported now.
+## IMPORTANT UPGRADE: samtools mpileup is deprecated, instead bcftools mpileup is used now.
 
 # The script will take a comma-separated list of THREE arguments.
-# The FIRST is the path to the reference genome to use. Only paths including the strings "hg19" or "hg38" are allowed, and they should point to exactly one of these genomes.
+# The FIRST is the path to the reference genome to use. Only paths including the strings "hg19" or "hg38" are allowed, and they should point to exactly one of these genomes. Chromosome names are expected to be simple "chr1" etc.
 # Obviously, it should be the same genome your sequences were mapped to. 
 # The SECOND is the species to test, only three inputs are supported: "pan" (chimpanzee or bonobo), "orang" or "gorilla".
-# The THIRD argument is the full path to a BAM file (or CRAM, if necessary), containing sequence alignments to the human reference genome.
+# The THIRD argument is the full path to a BAM file (or CRAM, if necessary), containing sequence alignments to the human reference genome. Check that these bam files are sorted.
 
 # CAUTION 2: Mapped reads should have aligned sequences/inserts of >= 35bp. Spurious alignments (for example, from bacteria) will be identical to the reference genome and appear as "contaminant".
 
